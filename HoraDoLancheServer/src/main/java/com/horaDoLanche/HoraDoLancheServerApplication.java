baf452a112d2461c488bc4422ec86ea891309d20
Keilane <keilanerizza@gmail.com>
2019-04-09 17:35:14 -0300

    Criando projeto Spring

---
+++ HoraDoLancheServer/src/main/java/com/horaDoLanche/HoraDoLancheServerApplication.java
@@ -0,0 +1,13 @@
+package com.horaDoLanche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoraDoLancheServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoraDoLancheServerApplication.class, args);
+	}
+
+}
